a41a6a3407ee62d15fe0a4e162b54134b688aa1a
dorvardi2 <dorvardi31@gmail.com>
2021-11-07 01:10:31 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -15,11 +15,20 @@ namespace Katzavia.Models
     {
         [Key]
         public int Id { get; set; }
+        [RegularExpression(@"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
+                   ErrorMessage = "Entered username format not valid.")]
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+                   ErrorMessage = "Entered Email format not valid.")]
         public string Email { get; set; }
         [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+           ErrorMessage = "Entered password format not valid,Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+   
         public UserType Type { get; set; } = UserType.Client;
         public UserInfo UserInfo { get; set; }
 
