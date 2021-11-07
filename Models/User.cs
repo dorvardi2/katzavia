117c5c852e29e199e30b22664afb9defed602d4b
EdenAlon1 <90115581+EdenAlon1@users.noreply.github.com>
2021-11-07 18:04:45 +0200

    FixLogin

--- Models/User.cs
+++ Models/User.cs
@@ -15,20 +15,14 @@ namespace Katzavia.Models
     {
         [Key]
         public int Id { get; set; }
-        [RegularExpression(@"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
-                   ErrorMessage = "Entered username format not valid.")]
         [Required]
+        [RegularExpression("^[a-zA-Z0-9._]*$", ErrorMessage = "Invalid name")]
         public string Username { get; set; }
-        [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
-                   ErrorMessage = "Entered Email format not valid.")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-           ErrorMessage = "Entered password format not valid,Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
         public UserType Type { get; set; } = UserType.Client;
         public UserInfo UserInfo { get; set; }
 
